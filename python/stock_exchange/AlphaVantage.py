b805655bddf749625b6fbe0670a21f4c49f3e9dc
weinchester <weinchester@gmail.com>
2019-09-19 16:56:04 +0800

    add new api for time series daily

--- python/stock_exchange/AlphaVantage.py
+++ python/stock_exchange/AlphaVantage.py
@@ -14,3 +14,9 @@ def get_stock_global_quote(symbol):
     return response.json()
 
 
+def get_timeseries_daily(symbol):
+    parameters['function'] = 'TIME_SERIES_DAILY'
+    parameters['symbol'] = symbol
+    parameters['outputsize'] = 'compact'
+    response = requests.get(api_url_base, params=parameters)
+    return response.json()
